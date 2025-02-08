06a2e183bf7ddbc816432d14e71e7dcfcdffb36c
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-08 22:39:58 +0200

    file compressor

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+from fabric import Connection, task
+import os
+import tarfile
+from datetime import datetime
+
+def do_pack():
+    """function that creates a .tgz archive from the contents of web_static"""
+
+    # ensure versions exitst or create it
+    os.makedirs("versions", exist_ok=True)
+
+    # creating a timestamp name
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"versions/web_static_{timestamp}.tgz"
+
+    with tarfile.open(archive_name, "w:gz") as tar:
+        tar.add("web_static", arcname="web_static")
+    return archive_name
---
+++ runme.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+time = datetime.now().strftime("%H:%M %Y")
+print(type(time))
