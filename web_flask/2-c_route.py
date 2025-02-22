f25b2fec993d131e011b9b906d4799e19f43fef0
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-22 22:44:15 +0200

    adding a new file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def index_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    content = text.replace("_", " ")
+    return f"C {content}"
+
+
+if __name__ == "__main__":
+    app.run()
