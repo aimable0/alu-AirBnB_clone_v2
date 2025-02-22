520591ca2b6bd6f4238b94d3ec12b1fde62c26b5
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-22 23:21:56 +0200

    fixing c_route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
 """
-a script that starts a Flask web application
+starts a Flask web application
 """
 
-from flask import Flask, url_for
-from markupsafe import escape
-
-
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def display_C(text):
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
     """display “C ” followed by the value of the text variable"""
-    text_underscore_removed = text.replace("_", " ")
-    return f"C {text_underscore_removed}"
+    return 'C ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
