d80300755765bd99e8ce8454691883bde10e3fca
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-24 14:49:13 +0200

    trial 06

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,7 +4,7 @@
             /: display “Hello HBNB!”
             /hbnb: display “HBNB”
             /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
-"""
+."""
 from flask import Flask
 from markupsafe import escape
 
