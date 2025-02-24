fb3adbfe688b621ecba18a2e95e49edcfe64936e
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-24 14:22:53 +0200

    trying fix 01

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,7 +5,7 @@
             /hbnb: display “HBNB”
             /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
 """
-from flask import Flask, url_for
+from flask import Flask
 from markupsafe import escape
 
 
@@ -14,19 +14,23 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Handling / route"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Handling /hnbn route"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def display_C(text):
+    """Handling /c/<text> route"""
     text_underscore_removed = text.replace("_", " ")
     return f"C {escape(text_underscore_removed)}"
 
 
 if __name__ == "__main__":
+    """running the app"""
     app.run(host="0.0.0.0", port=5000)
