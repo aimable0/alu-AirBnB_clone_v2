e4d9cc524d359454a2d7d3d188c21ecf52f88477
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-24 13:40:58 +0200

    refixing code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
-#!/usr/bin/python3
-"""  a script that starts a Flask web application """
 from flask import Flask
 
+"""A script that starts a flask app that displays 'Hello HBNB' """
 app = Flask(__name__)
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello():
-    """ display “Hello HBNB!” """
+@app.route("/", strict_slashes=False)
+def index():
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
