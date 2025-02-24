6d9a09c0436cc229afd3c4e1a3780aabbd738c6f
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-24 16:34:38 +0200

    trial 01

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,7 +3,8 @@
     Routes:
             /: display “Hello HBNB!”
             /hbnb: display “HBNB”
-            /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+            /c/<text>: display “C ” followed by the value of the text variable
+            (replace underscore _ symbols with a space )
 """
 from flask import Flask
 
