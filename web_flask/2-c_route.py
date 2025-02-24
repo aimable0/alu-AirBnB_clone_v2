1eb2a175da8c409eae0b17c37808f8b9df8aab47
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-24 14:39:24 +0200

    trying to fix documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application:
-    Routes:
-            /: display “Hello HBNB!”
-            /hbnb: display “HBNB”
-            /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
-"""
+"""Script that starts a Flask web application."""
 from flask import Flask
-from markupsafe import escape
-
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
-    """Handling / route"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Handling /hnbn route"""
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def display_C(text):
-    """Handling /c/<text> route"""
-    text_underscore_removed = text.replace("_", " ")
-    return f"C {escape(text_underscore_removed)}"
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    return 'C ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
-    """running the app"""
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
