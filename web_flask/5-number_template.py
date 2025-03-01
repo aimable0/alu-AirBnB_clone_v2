b167928bcbba43925d51c1ffbba974be91447b21
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-03-01 14:17:02 +0200

    new trial

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,36 +7,37 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """Handling / route"""
+    """Returns Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Handling /hnbn route"""
+    """Returns HBNB"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def display_C(text):
-    """Handling /c/<text> links"""
+    """Returns C <text>"""
     return "C " + text.replace("_", " ")
 
 
 @app.route("/python/<text>", strict_slashes=False)
 def display_python(text):
-    """Handling /python/<text> links"""
+    """Returns Python <text>"""
     return "Python " + text.replace("_", " ")
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
-def display_number(n):
-    """handling /number/n routes"""
+def number(n):
+    """Returns <n> is a number"""
     return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def display_html(n):
+def number_template(n):
+    """Returns HTML page if n is an integer"""
     return render_template("5-number.html", n=n)
 
 
