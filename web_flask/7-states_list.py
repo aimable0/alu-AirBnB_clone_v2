03156bf1ce9b0948710040402ea34603a480b45b
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-03-03 23:14:32 +0200

    fix 0

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,25 +2,27 @@
 
 """Starts a Flask web application"""
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def return_states():
-    """return all states objects from storage"""
-    states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """Comment"""
+    states = storage.all('State').values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def close_storage(self):
-    """Removes the current SQLAlchemy session"""
+def teardown(self):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
