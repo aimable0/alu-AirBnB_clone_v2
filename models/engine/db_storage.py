46cdcb82a25f4f7afed7b2bcb5bc37bacfe700e3
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-03-03 20:14:45 +0200

    removing trailing whitespace

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -95,7 +95,7 @@ class DBStorage:
 
         Base.metadata.create_all(bind=self.__engine)
         session_factory = sessionmaker(
-            bind=self.__engine, 
+            bind=self.__engine,
             expire_on_commit=False
         )
         Session = scoped_session(session_factory)
