2a61f1a4d2116bbb8ddb959228d9956b553be6c9
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-03-04 21:31:33 +0200

    trial 13

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,10 +3,16 @@
 
 from flask import Flask
 from models import storage
-from models.city import City
 from models.state import State
 
 app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def return_filters():
+    states = storage.all(State)
+    return ""
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
